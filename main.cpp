cc66328c7539b4fd01fa4e52e6d013dca10eef8f
Hanno Hennighausen <Hanno.Henninghausen@synmikro.mpi-marburg.mpg.de>
2021-10-21 11:38:05 +0200

    update 19.10

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-# include <fstream>
+ # include <fstream>
 # include <cmath>
 # include <iostream>	// cout, etc.
 #include <sstream>      // std::stringstrea
@@ -6,47 +6,75 @@
 #include <vector>
 using namespace std;
 
-// fct takes position of diffuse particles attempts to move them,
+// fct takes position of diffuse particles attempts to move one at random,
 //if move goes to a lattice point without neighbours then move is accepted
-//if move attempts to move to next to a particle --> return particle combination for step 3
-// RETURN, updated diffuse_pos and grid, create array holding attempted binding
-int diffuse(vector diffuse_pos,array grid){
+//if move attempts to move next to a particle --> return particle combination for step 3
+// RETURN, updated diffuse_pos and grid, create array with attempted binding
+void diffuse(vector diffuse_pos,array grid){
 
-return 0;
+
 }
 
-// find empty hexes
-// randomely create particles with k_on
+// find empty hexes without neighbours
+// randomely create a particle with k_on/
 //RETURN updated grid and diffuse position
-int create_particle(vector diffuse_pos, vector bound_pos, array grid){
 
-return 0;
+//Do I need to check how full the grid is? Set concentration?delete particles randomely?
+void create_particle(vector diffuse_pos, vector bound_pos, array grid){
+int const k_on = 0.02;
+
+
 }
+
+
+
 
 //calculate energy change by binding/unbinding a particle
 //
-int energy_change(vector bound_pos, array grid){
+void energy_change(vector bound_pos, array grid){
+int const alpha=1;
+int const J=1;
+int no_bound==bound_pos.size()
 
-return 0;
+float nucleation_term = alpha*J*no_bound
+
+
 }
 
-int main(){
-const int N = 10; // number of Monte Carlo Steps
-int n = 0;
 
-while (n<N){
+
+
+
+
+
+
+int main(){
+const int MC_steps = 10; // number of Monte Carlo Steps
+int MC_counter = 0;
+
+while (MC_counter<MC_Steps){
 
 cout << n <<'\n';
 //step 1: Move diffusive particles
 
 diffuse(a,b);
+//if move is rejected check if it binds or not
 
-//step 2: Check and create new particles
 
+
+
+//step 2: Check and create a new particles
+
+create_particle(a,c,b);
 
 //step 3: bind and unbind
+//first loop through the array for from step 1 to check if diffusive particles bind/form a new cluster or not
 
-n++;
+
+
+
+
+MC_counter++;
 
 
 
