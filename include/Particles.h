d74f68c9323efef2ee2c223d2ad95e2afd0b4479
Srikanth Subramanian <sssrikanth92@gmail.com>
2023-03-10 15:52:19 +0100

    two_particle model works

--- include/Particles.h
+++ include/Particles.h
@@ -40,51 +40,6 @@ void print_container(const std::vector<int>& c)
     std::cout << '\n';
 }
 
-const bool Frz_Interaction_Lookuptable[6*32]=
-{
-    true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,
-    true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,
-    true,true,true,true,false,false,false,false,true,true,true,true,false,false,false,false,
-    true,true,false,false,true,true,false,false,true,true,false,false,true,true,false,false,
-    true,true,false,false,true,true,false,false,true,true,false,false,true,true,false,false,
-    true,true,true,true,false,false,false,false,true,true,true,true,false,false,false,false,
-    true,true,false,false,true,true,false,false,true,true,false,false,true,true,false,false,
-    true,true,true,true,false,false,false,false,true,true,true,true,false,false,false,false,
-    true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,
-    true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,
-    true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,
-    true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false
-};
-//00-15:   slope 0 ori -1,0 Frz_flag 0,0 0,1 0,2 0,3 1,0 1,1 1,2...
-//16-31:   slope 0 ori 0,-1 Frz_flag 0,0 0,1 0,2 0,3 1,0 1,1 1,2...
-//32-47:   slope 1 ori -1,1 Frz_flag 0,0 0,1 0,2 0,3 1,0 1,1 1,2...
-//48-63:   slope 1 ori 1,-1 Frz_flag 0,0 0,1 0,2 0,3 1,0 1,1 1,2...
-//64-79:   slope 2 ori 0,1  Frz_flag 0,0 0,1 0,2 0,3 1,0 1,1 1,2...
-//80-95:   slope 2 ori 1,0  Frz_flag 0,0 0,1 0,2 0,3 1,0 1,1 1,2...
-//96-111:  slope 3 ori -1,0 Frz_flag 0,0 0,1 0,2 0,3 1,0 1,1 1,2...
-//112-127: slope 3 ori 0,-1 Frz_flag 0,0 0,1 0,2 0,3 1,0 1,1 1,2...
-//128-143: slope 4 ori -1,1 Frz_flag 0,0 0,1 0,2 0,3 1,0 1,1 1,2...
-//144-159: slope 4 ori 1,-1 Frz_flag 0,0 0,1 0,2 0,3 1,0 1,1 1,2...
-//160-175: slope 5 ori 0,1  Frz_flag 0,0 0,1 0,2 0,3 1,0 1,1 1,2...
-//176-191: slope 5 ori 1,0  Frz_flag 0,0 0,1 0,2 0,3 1,0 1,1 1,2...
-
-const bool Frz_occupied_site[6*12]=
-{
-    false,false,false,false,true,false,true,false,false,false,false,false,
-    true,true,false,false,false,false,false,false,false,false,false,false,
-    false,false,false,false,false,false,false,false,true,true,false,false,
-    false,false,false,false,true,true,false,false,false,false,false,false,
-    true,false,true,false,false,false,false,false,false,false,false,false,
-    false,false,false,false,false,false,false,false,true,false,true,false
-};
-
-//00-11 slope 0 possible directions for Frz B to point ori order -1,0,1
-//12-23 slope 1
-//24-35 slope 2
-//36-47 slope 3
-//48-59 slope 4
-//60-72 slope 5
-
 
 struct Interactions
 {
@@ -92,6 +47,8 @@ struct Interactions
     std::vector<int> orientations;
     int num_diffuse;
     int num_bonds;
+    int num_aa_bonds;
+    int num_ab_bonds;
 };
 
 struct Particle
@@ -100,7 +57,6 @@ struct Particle
     int ori=0;
     int Frz_A_B=0;
 };
-
 
 
 
@@ -175,7 +131,7 @@ public:
             {
                 auto p = std::make_shared<Particle>();
                 p-> pos = pos;
-                p->ori =1;
+                p-> ori =1;
 
                 particles[i] = p;
                 std::weak_ptr<Particle> pw(p);
@@ -248,108 +204,7 @@ public:
         }
     }
 
-    inline std::vector<int> get_free_flag_sites(const int pos)
-    {
-        std::vector<int> free_sites;
-        int ori = get_orientation(pos);
-        int frz = get_flag(pos);
-        std::vector<Neighbour> n(lattice.get_neighbors2(pos));
 
-        if(ori==0)
-        {
-            auto it1=(std::find_if(begin(n),end(n), [](Neighbour m)
-            {
-                return m.slope==0;
-            }));
-            if(it1!=n.end())
-            {
-                int n_ind = it1-n.begin();
-                if(grid1[n[n_ind].position].expired())
-                {
-                    int pos_neighbor1=n[n_ind].position;
-                    free_sites.emplace_back(2);
-                }
-            }
-
-            auto it2=(std::find_if(begin(n),end(n), [](Neighbour m)
-            {
-                return m.slope==3;
-            }));
-            if(it2!=n.end())
-            {
-                int n_ind = it2-n.begin();
-                if(grid1[n[n_ind].position].expired())
-                {
-                    int pos_neighbor2=n[n_ind].position;
-                    free_sites.emplace_back(1);
-                }
-            }
-        }
-        else if (ori==1)
-        {
-            auto it1=(std::find_if(begin(n),end(n), [](Neighbour m)
-            {
-                return m.slope==2;
-            }));
-            if(it1!=n.end())
-            {
-                int n_ind = it1-n.begin();
-                if(grid1[n[n_ind].position].expired())
-                {
-                    int pos_neighbor1=n[n_ind].position;
-                    free_sites.emplace_back(1);
-                }
-            }
-
-            auto it2=(std::find_if(begin(n),end(n), [](Neighbour m)
-            {
-                return m.slope==5;
-            }));
-            if(it2!=n.end())
-            {
-                int n_ind = it2-n.begin();
-                if(grid1[n[n_ind].position].expired())
-                {
-                    int pos_neighbor2=n[n_ind].position;
-                    free_sites.emplace_back(2);
-                }
-            }
-        }
-
-        else if(ori==-1)
-        {
-            auto it1=(std::find_if(begin(n),end(n), [](Neighbour m)
-            {
-                return m.slope==1;
-            }));
-            if(it1!=n.end())
-            {
-                int n_ind = it1-n.begin();
-                if(grid1[n[n_ind].position].expired())
-                {
-                    int pos_neighbor1=n[n_ind].position;
-                    free_sites.emplace_back(2);
-                }
-            }
-
-            auto it2=(std::find_if(begin(n),end(n), [](Neighbour m)
-            {
-                return m.slope==4;
-            }));
-            if(it2!=n.end())
-            {
-                int n_ind = it2-n.begin();
-                if(grid1[n[n_ind].position].expired())
-                {
-                    int pos_neighbor2=n[n_ind].position;
-                    free_sites.emplace_back(1);
-                }
-            }
-        }
-
-
-        return free_sites;
-    }
 
     inline void set_orientation(const int pos, const int ori)
     {
@@ -378,47 +233,13 @@ public:
 //            p-> Frz_A_B = frz;
 //        }
     }
+
     inline bool is_interaction_allowed(const int ori_self, const int ori_other, const int flag_self, const int flag_other, const int slope) const
     {
-        int order_of_flags;
-        if(ori_self<ori_other)
-        {
-            order_of_flags=0;
-        }
-        else
-        {
-            order_of_flags=16;
-        }
-        int key = slope*32 + order_of_flags + flag_self*4 + flag_other;
-//        bool b = (ori_self != ori_other && ((ori_self + (slope%3-1))*(ori_other+(slope%3-1)))!=0 && Frz_Interaction_Lookuptable[key]);
-        return (ori_self != ori_other && ((ori_self + (slope%3-1))*(ori_other+(slope%3-1)))!=0 && Frz_Interaction_Lookuptable[key]);
-//        return (ori_1 != ori_2 && ((ori_1 + slope)*(ori_2+slope))!=0);
+
+        return (ori_self != ori_other && ((ori_self + (slope%3-1))*(ori_other+(slope%3-1)))!=0 && (flag_self*flag_other!=1));
     }
 ////////////////////////////////////////////////////////////
-
-//// needs fixing
-//    inline bool is_occupied_by_FrzB(const int pos) const
-//    {
-//        std::vector<Neighbour> n(lattice.get_neighbors2(pos));
-//
-//        for(unsigned int i=0; i<n.size(); i++)
-//        {
-//            if(is_bound(n[i].position))
-//            {
-//                int key=n[i].slope*12+(get_orientation(n[i].position)+1)*4+get_flag(n[i].position);
-//                bool b= Frz_occupied_site[key];
-//                if(b)
-//                {
-//                    return true;
-//                }
-//            }
-//        }
-//        return false;
-//    }
-    inline bool is_frz_interaction_allowed(const int flag_1, const int flag_2, const int slope) const
-    {
-        return (flag_1!=1 && flag_2!=1);
-    }
 
     //COUNTING THE BOUND PARTICLES OF A GIVEN POSITION
     int count_interacting_neighbors(const int pos, const int ori) const
@@ -432,7 +253,6 @@ public:
             {
                 count_bound++;
             }
-
         }
         assert(count_bound>=0);
         return count_bound;
@@ -578,16 +398,16 @@ public:
 
 
 //DELTA_H
-    double delta_H(double const alpha, double const J, int num_dif, int num_bonds, double a) const
+    double delta_H(double const alpha, double const J, int num_dif, int num_aa_bonds, int num_ab_bonds, double a, double beta) const
     {
-        //std::cout<<"num_dif "<<num_dif<<"\n num_bonds "<< num_bonds<<"\n";
-        double delta_E=(a*J)*(num_bonds-(num_dif*alpha));
+//        std::cout<<"num_dif "<<num_dif<<"\t num_aa_bonds "<< num_aa_bonds<<"\t"<<num_ab_bonds<<"\n";
+        double delta_E=(a*J)*(num_aa_bonds-(num_dif*alpha)) - beta*num_ab_bonds;
         return delta_E<0.0f ? 1.0 : exp(-delta_E);
     }
 
 
 //BINDING ATTEMPT
-    void attempt_binding(double const alpha, double const J, int ind, double &rand)
+    void attempt_binding(double const alpha, double const J,double const beta, int ind, double &rand)
     {
         int pos = get_pos(ind);
         double rand_size = rand*3;
@@ -597,8 +417,9 @@ public:
 
         Interactions interactions;
         interactions.num_bonds=0;
+        interactions.num_aa_bonds=0;
+        interactions.num_ab_bonds=0;
         interactions.num_diffuse=1;
-
 
         std::vector<Neighbour> n(lattice.get_neighbors2(pos));
 
@@ -606,6 +427,7 @@ public:
         {
             return is_diffuse(n.position);
         };
+
         auto _get_compatible_diffuse_neighbours = [&interactions,this,&rand,ori,pos](auto n)
         {
             double rand_size = rand*3;
@@ -616,7 +438,16 @@ public:
             {
                 interactions.possible_interaction_pos.emplace_back(n.position);
                 interactions.orientations.emplace_back(ori2);
-                interactions.num_bonds++;
+
+                if((get_flag(pos)==0 && get_flag(n.position)==0))              //check if aa is interacting or ab is interacting
+                {
+                    interactions.num_aa_bonds++;
+                }
+                else
+                {
+                    interactions.num_ab_bonds++;
+                }
+
                 interactions.num_diffuse++;
                 int bound_neigh_of_neigh = count_interacting_neighbors(n.position,ori);
                 if(bound_neigh_of_neigh>1)
@@ -626,7 +457,7 @@ public:
             }
         };
 
-        auto s= n | std::views::filter(_is_diffuse);
+        auto s = n | std::views::filter(_is_diffuse);
         std::ranges::for_each(s,_get_compatible_diffuse_neighbours);
 
 
@@ -638,24 +469,34 @@ public:
         {
             return is_interaction_allowed(ori,get_orientation(n.position),get_flag(pos),get_flag(n.position),n.slope);
         };
-        auto _get_affected_particles = [&interactions,this](auto n)
+        auto _get_affected_particles = [&interactions,this,pos](auto n)
         {
             interactions.orientations.emplace_back(get_orientation(n.position));
             interactions.possible_interaction_pos.emplace_back(n.position);
-            interactions.num_bonds++;
+
+            if((get_flag(pos)==0 && get_flag(n.position)==0))              //check if aa is interacting or ab is interacting
+            {
+                interactions.num_aa_bonds++;
+            }
+            else
+            {
+                interactions.num_ab_bonds++;
+            }
+
         };
+//        std::cout<<interactions.num_aa_bonds<<'\n';
 
         auto s2= n | std::views::filter(_is_bound) | std::views::filter(_is_allowed);
         std::ranges::for_each(s2,_get_affected_particles);
 
 
 
-        if(interactions.num_bonds>0)
+        if((interactions.num_aa_bonds+interactions.num_ab_bonds)>0)
         {
             assert(interactions.possible_interaction_pos.size()>0);
             binding_attempt++;
-            double delta = delta_H(alpha,J,interactions.num_diffuse,interactions.num_bonds, -1);
-            //std::cout<<"delta "<< delta << "\n";
+            double delta = delta_H(alpha,J,interactions.num_diffuse,interactions.num_aa_bonds,interactions.num_ab_bonds, -1,beta);
+//            std::cout<<"delta "<< delta << "\n";
             //std::cout<<"\n rand "<<rand;
             //the binding attempt is succesful if rand < delta_E
             if(rand<delta)
@@ -692,7 +533,7 @@ public:
 
 
 //UNBINDING ATTEMPT
-    void attempt_unbinding(double const alpha, double const J, int ind,double &rand)
+    void attempt_unbinding(double const alpha, double const J,double const beta, int ind,double &rand)
     {
         // get bound particle
         unbinding_attempt++;
@@ -720,6 +561,8 @@ public:
 
         Interactions interactions;
         interactions.num_bonds=0;
+        interactions.num_aa_bonds=0;
+        interactions.num_ab_bonds=0;
         interactions.num_diffuse=1;
 
 
@@ -733,9 +576,17 @@ public:
         {
             return is_interaction_allowed(ori,get_orientation(n.position),get_flag(pos),get_flag(n.position),n.slope);
         };
-        auto _get_affected_particles = [&interactions,this](auto n)
+        auto _get_affected_particles = [&interactions,this,pos](auto n)
         {
-            interactions.num_bonds++;
+
+            if((get_flag(pos)==0 && get_flag(n.position)==0))              //check if aa is interacting or ab is interacting
+            {
+                interactions.num_aa_bonds++;
+            }
+            else
+            {
+                interactions.num_ab_bonds++;
+            }
             int bound_neigh_of_neigh = count_interacting_neighbors(n.position,get_orientation(n.position));
             if(bound_neigh_of_neigh==1)
             {
@@ -747,9 +598,7 @@ public:
         auto s= n | std::views::filter(_is_bound) | std::views::filter(_is_allowed);
         std::ranges::for_each(s,_get_affected_particles);
 
-
-
-        double delta_E =delta_H(alpha,J,interactions.num_diffuse,interactions.num_bonds, 1);
+        double delta_E =delta_H(alpha,J,interactions.num_diffuse,interactions.num_aa_bonds,interactions.num_ab_bonds, 1,beta);
 
         if (rand<delta_E)
         {
@@ -773,103 +622,50 @@ public:
 
 
 // Binding FrzB acceptance rate 1
-    void binding_FrzB(int ind, int &FrzB_num, double &rand)
+    void binding_FrzB(int ind, int &FrzB_num)
     {
 //        assert (rand<1 && rand>0);
         if (FrzB_num>0)
         {
-
-
         int pos=get_pos(ind);
         int frz = get_flag(pos);
-//        std::cout<<"FrzB_num \t"<<FrzB_num<<"\t"<<"pos frz \t"<< pos<<"\t"<<frz<<"\t"<<"rand \t"<<rand<<"\t";
-//        double rate_acceptance = FrzB_num/(double)420;//(Nxy-particles.size());
-        double rate_acceptance = 1;//(Nxy-particles.size());
 
-//        std::cout<<"rate_acc \t"<<rate_acceptance<<"\t";
-
-        if(is_diffuse(pos))
-        {
-            if(frz==3)
-            {
-                return;
-            }
-            else if(frz==0 && rand<rate_acceptance)
-            {
-                int new_frz = unidist(gen)*2;
-                FrzB_num--;
-                set_frz(ind,new_frz+1);
-//                std::cout<<1<<"\t";
-            }
-            else if(frz>0 && frz<3 && rand<rate_acceptance)
-            {
-                FrzB_num--;
-                set_frz(ind,3);
-//                std::cout<<2<<"\t";
-            }
-        }
-        else if(is_bound(pos))
-        {
-            if(frz==3)
-            {
-                return;
-            }
-            else if(frz==0 && rand<rate_acceptance )
-            {
-                std::vector<int> free_sites(get_free_flag_sites(pos));
-                if(free_sites.size()==1 && rand<rate_acceptance)
-                {
-                    set_frz(ind, free_sites[0]);
-                    FrzB_num--;
-//                    std::cout<<4<<"\t";
-                }
-                else if(free_sites.size()==2 && rand<rate_acceptance)
-                {
-                    int new_frz = unidist(gen)*2;
-                    set_frz(ind,new_frz+1);
-                    FrzB_num--;
-//                    std::cout<<5<<"\t";
-                }
-            }
-            else if(frz>0 && frz<3 && rand<rate_acceptance)
-            {
-                std::vector<int> free_sites(get_free_flag_sites(pos));
-                if(free_sites.size()==2 && rand<rate_acceptance)
-                {
-                    set_frz(ind,3);
-                    FrzB_num--;
-//                    std::cout<<6<<"\t";FrzB_{run}_image_J_{J}_alpha_{alpha}_FrzB_{FrzB}_off_{rate}')
-                }
-            }
-        }
-        }
-//        std::cout<<FrzB_num<<"\n";
-    }
-// Unbinding FrzB acceptance rate proportional to e^(-deltaE)=const
-    void unbinding_FrzB(const int ind,int &FrzB_num, const double &rate, double &rand )
-    {
-//        return;
-        int pos=get_pos(ind);
-        int frz = get_flag(pos);
-        if(frz==0)
+        if(frz==1)
         {
             return;
         }
-
-        else if(frz>0 && frz<3 && rand<rate)
+        else if(frz==0)
         {
-            FrzB_num++;
-            set_frz(ind,0);
-            //options are to go from one to no FrzB
+            FrzB_num--;
+            set_frz(ind,1);
         }
-        else if(frz==3 && (rate*2)>rand)
-        {
-            int new_frz = unidist(gen)*2;
-            FrzB_num++;
-            set_frz(ind,new_frz+1);
-            //options are no FrzB = 0, or one FrzB = 1 or 2
         }
     }
+// Unbinding FrzB acceptance rate proportional to e^(-deltaE)=const
+//    void unbinding_FrzB(const int ind,int &FrzB_num, const double &rate, double &rand )
+//    {
+////        return;
+//        int pos=get_pos(ind);
+//        int frz = get_flag(pos);
+//        if(frz==0)
+//        {
+//            return;
+//        }
+//
+//        else if(frz>0 && frz<3 && rand<rate)
+//        {
+//            FrzB_num++;
+//            set_frz(ind,0);
+//            //options are to go from one to no FrzB
+//        }
+//        else if(frz==3 && (rate*2)>rand)
+//        {
+//            int new_frz = unidist(gen)*2;
+//            FrzB_num++;
+//            set_frz(ind,new_frz+1);
+//            //options are no FrzB = 0, or one FrzB = 1 or 2
+//        }
+//    }
 
 
 // labeling a cluster
@@ -1007,8 +803,10 @@ public:
 
         for(auto &x:Frz_vector)
         {
-            buffer << x<< '\t';
+//            std::cout<<x<<'\t';
+            buffer <<x<< '\t';
         }
+//        std::cout<<'\n';
         buffer << '\n';
         out << buffer.str();
     }
--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@
 
 int main(int argc,char *argv[])
 {
-    double J,alpha,rate;
+    double J,alpha,beta,rate;
     int slurm_index;
     int FrzB_num;
     if(argc==6)
@@ -34,16 +34,17 @@ int main(int argc,char *argv[])
     {
         J = 4;
         alpha=0.5;
+        beta = J+2;
         FrzB_num=28;
         rate=0.03;
-        slurm_index = 26;
+        slurm_index = 4;
         std::cout << "Using default parameters." << '\n';
     }
 
     titration_concentration_frzb = 1;
     density = 0.2;
 
-    const long int MC_steps = 2*pow(10,6); // number of Monte Carlo Steps
+    const long int MC_steps = 4*pow(10,6); // number of Monte Carlo Steps
     long int MC_counter = 0;
 //    long double rand;
     double rand;
@@ -110,7 +111,7 @@ int main(int argc,char *argv[])
 //            assert(rand<1);
             for(unsigned int j=0;j<temp_FrzB_num;j++)
             {//BINDING OF FrzB
-                particles.binding_FrzB(j, FrzB_num, rand);
+                particles.binding_FrzB(j, FrzB_num);
                 std::cout<<FrzB_num<<'\n';
             }
 
@@ -129,24 +130,8 @@ int main(int argc,char *argv[])
 
         for(unsigned int attempted_moves=0; attempted_moves<particles.particles.size(); attempted_moves++)
         {
-//        std::cout <<"counter"<< MC_counter <<'\n';
-////////////////// FrzB Un-/Binding Section //////////////////////////
 
-//      choose random particle for FrzB binding/unbinding
-            rand = unidist(gen);
-            rand_size = rand*particles.particles.size();
-            int ind=rand_size;
-            rand = rand_size-ind;
-            assert(rand<1);
-            //BINDING OF FrzB
-            particles.binding_FrzB(ind, FrzB_num, rand);
 
-            rand = unidist(gen);
-            rand_size = rand*particles.particles.size();
-            ind=rand_size;
-            rand = rand_size-ind;
-            //UNBINDING OF FrezB
-            particles.unbinding_FrzB(ind, FrzB_num,rate, rand);
 //////////////////////////////////////////////////////////////////////
 
 //////////////////// Particle Movement, Un-/Binding to Clusters etc ///
@@ -154,7 +139,7 @@ int main(int argc,char *argv[])
 //      choose random particle for interaction and movement
             rand = unidist(gen);
             rand_size = rand*particles.particles.size();
-            ind=rand_size;
+            int ind=rand_size;
             rand=rand_size-ind;
             assert(rand<1);
             if(particles.is_diffuse(particles.get_pos(ind)))
@@ -163,13 +148,13 @@ int main(int argc,char *argv[])
                 particles.attempt_diffusion(ind, rand);
 
 //BINDING
-                particles.attempt_binding(alpha,J,ind,rand);
+                particles.attempt_binding(alpha,beta,J,ind,rand);
             }
 
             else if(particles.is_bound(particles.get_pos(ind)))
             {
 // UNBINDING ATTEMPT
-                particles.attempt_unbinding(alpha,J,ind,rand);
+                particles.attempt_unbinding(alpha,beta,J,ind,rand);
             }
 //////////////////////////////////////////////////////////////////////////
         }
